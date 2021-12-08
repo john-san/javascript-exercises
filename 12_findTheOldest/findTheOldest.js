df9c9ec9a8357b5684d34a828d61016eb1fa5507
John Nguyen <npjohn92@gmail.com>
2021-12-08 08:08:10 -0800

    add solutions for 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr
+    .reduce((a,b) => {
+    if (!a.hasOwnProperty('yearOfDeath')) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (!b.hasOwnProperty('yearOfDeath')) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
 
+    const aAge = a.yearOfDeath - a.yearOfBirth;
+    const bAge = b.yearOfDeath - b.yearOfBirth;
+
+    return aAge > bAge ? a : b;
+  });
 };
 
 // Do not edit below this line
